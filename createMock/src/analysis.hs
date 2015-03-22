9ac4528f5dab577a31f2d12bafc2d9283e620e7f
minddnim <minddnim@gmail.com>
2015-03-22 23:11:47 +0900

    getRecursiveContents implementation

--- createMock/src/analysis.hs
+++ createMock/src/analysis.hs
@@ -1,3 +1,22 @@
+module RecursiveContents (getRecursiveContents) where
+
+import Control.Monad (forM)
+import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.FilePath((</>))
+
+getRecursiveContents :: FIlePath -> IO [FIlePath]
+getRecursiveContents topdir = do
+  names <- getDirectoryContents topdir
+  let properNames = filter (`notElem` [".", ".."]) names
+  paths <- forM properNames $ /name -> do
+    let path = topdir </> name
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+      then getRecursiveContents path
+      else return [path]
+  return (concat paths)
+
+
 main :: IO()
 main = do
   src <- readFile "../testSrc/test.c"
@@ -6,4 +25,4 @@ main = do
 
 createMock :: String -> String
 createMock str = undefined
-  
+
